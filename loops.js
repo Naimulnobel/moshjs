27e2e9dd39248bafa7408c4442a2d05add666005
naimulnobel <nobelnaimulislam@gmail.com>
2022-03-30 20:44:20 +0600

    first commit

---
+++ loops.js
@@ -0,0 +1,49 @@
+// for (let i = 0; i < 5; i++) {
+//     console.log(i)
+// }
+// let i = 0
+// while (i <= 5) {
+//     console.log(i)
+//     i++
+// }
+// let i = 9
+// do {
+//     if (i % 2 !== 0) console.log(i)
+// } while (i <= 5)
+// const person = {
+//     name: 'nobel',
+//     age: 26
+// }
+// for (let key in person) {
+//     console.log(person[key])
+// }
+// const colors = ['green', 'red', 'blue', 'white']
+// for (let index in colors) {
+//     console.log(colors[index])
+// }
+// for (let color of colors)
+//     console.log(color)
+// let i = 0
+// while (i <= 5) {
+//     if (i % 2 === 0) {
+//         i++;
+//         continue
+//     }
+//     console.log(i)
+//     i++
+// }
+// function max(number1, number2) {
+//     // if (number1 > number2) {
+//     //     return number1
+//     // }
+//     // return number2
+
+//     return number1 > number2 ? number1 : number2
+// }
+// const maxNumber = max(10, 20)
+// console.log(maxNumber)
+// function landscape(width, height) {
+//     return width > height ? 'landscape' : 'portrait'
+// }
+// const landscape1 = landscape(10, 20)
+// console.log(landscape1)
