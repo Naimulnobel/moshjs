d39abb4c4f75f38d93df22b3b5b5944a47d38da0
naimulnobel <nobelnaimulislam@gmail.com>
2022-03-31 15:37:41 +0600

    objects

--- objects.js
+++ objects.js
@@ -1,22 +1,45 @@
 
 // circle.draw();//method
 // Factory Function
-// function createCircle(radius){
-//     return {
-//         radius:radius,
-//         draw(){
-//             console.log('draw')
-//         }
-//     }
-// }
+function createCircle(radius){
+    return {
+        radius:radius,
+        draw(){
+            console.log('draw')
+        }
+    }
+}
 
-//Constructor Function
-// function Circle(radius){
-//     this.radius = radius;
-//     this.draw = function(){
-//         console.log('draw')
-//     }
-// }
+// Constructor Function
+function Circle(radius){
+    this.radius = radius;
+    this.draw = function(){
+        console.log('draw')
+    }
+}
 // const circle= new Circle(1)
 // console.log(circle)
 
+
+const circle={
+    radius:1,   
+    draw(){
+        console.log('draw')
+    }
+}
+// for(let key of Object.keys(circle)){
+//     console.log(key,circle[key])
+// }
+// for(let entry of Object.entries(circle)){
+//     console.log(entry)
+// }
+
+//colning an odject
+// const another ={}
+// for(let key in circle){
+//     another[key]=circle[key]
+// }
+const another={...circle}||Object.assign({},circle)
+console.log(another)
+
+//MATH OBJECT
