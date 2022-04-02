6212555c3559b4e13a990734b7128e20b49c91c5
naimulnobel <nobelnaimulislam@gmail.com>
2022-04-02 15:12:04 +0600

    array

---
+++ array.js
@@ -0,0 +1,38 @@
+const numbers=[3,4]
+// end of array
+numbers.push(5,6,7)
+//Beginning of array
+numbers.unshift(1,2)
+//middle of array
+numbers.splice(2,0, 'a','b')
+console.log(numbers)
+//findin index of array
+console.log(numbers.indexOf('a'))
+//element in array
+console.log(numbers.includes(1))
+
+const courses = [
+    {id:1, name:'a'},
+    {id:2, name:'b'},
+]
+const course = courses.find((course)=>{
+    return course.name === 'a'
+})
+const course1 = courses.findIndex((course)=>{
+    return course.name === 'a'
+})
+console.log(course1)
+
+//remove element from array
+//end element in array
+numbers.pop()
+//beginning element in array
+numbers.shift()
+//remove element from array
+numbers.splice(2,1)
+//empty array
+numbers.length=0
+const first=[1,2,3]
+const second=[4,5,6]
+const combined =first.concat(second)
+const slice =combined.slice(2,4)
