e1f76c7e7818e82a62c55b5fce7b46cfe5a44891
naimulnobel <nobelnaimulislam@gmail.com>
2022-04-06 10:50:02 +0600

    sorting

--- array.js
+++ array.js
@@ -5,11 +5,11 @@ numbers.push(5,6,7)
 numbers.unshift(1,2)
 //middle of array
 numbers.splice(2,0, 'a','b')
-console.log(numbers)
+// console.log(numbers)
 //findin index of array
-console.log(numbers.indexOf('a'))
+// console.log(numbers.indexOf('a'))
 //element in array
-console.log(numbers.includes(1))
+// console.log(numbers.includes(1))
 
 const courses = [
     {id:1, name:'a'},
@@ -21,7 +21,7 @@ const course = courses.find((course)=>{
 const course1 = courses.findIndex((course)=>{
     return course.name === 'a'
 })
-console.log(course1)
+console.log(course)
 
 //remove element from array
 //end element in array
@@ -34,5 +34,18 @@ numbers.splice(2,1)
 numbers.length=0
 const first=[1,2,3]
 const second=[4,5,6]
-const combined =first.concat(second)
+// const combined =first.concat(second)
+const combined =[...first, ...second]
 const slice =combined.slice(2,4)
+console.log(slice)
+const numberss = [1,2,3,4,5,6,7,8,9,10]
+numberss.forEach((number,index)=>{
+    console.log(index,number)
+})
+const joined=numberss.join(',')
+console.log(joined)
+const message = 'This is a message'
+const parts = message.split(' ')
+console.log(parts)
+const combined1 = parts.join('-')
+console.log(combined1)
