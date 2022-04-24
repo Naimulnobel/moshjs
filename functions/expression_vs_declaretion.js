a79553e4822d7dc8cc590a143454054c94fc9baa
naimulnobel <nobelnaimulislam@gmail.com>
2022-04-24 17:57:10 +0600

    function

---
+++ functions/expression_vs_declaretion.js
@@ -0,0 +1,9 @@
+//function Declaration
+function walk() {
+  console.log("walk");
+}
+//function expression
+const run = function () {
+  console.log("run");
+};
+//hoisting javascript engine will move the function declaration to the top of the file
